999c877415609e84f2d998c8c294edc896ed7f8f
acleyfilho <50428838+acleyfilho@users.noreply.github.com>
2020-11-04 19:44:24 -0300

    Inclusão da classe Cozinha
    Foi desenvolvida a classe cozinha do sistema de drive-thru

---
+++ Cozinha.cs
@@ -0,0 +1,29 @@
+class Cozinha{
+  private int _tempoPreparo;
+
+  public Cozinha(){
+
+  }
+
+  public Cozinha(int tempo){
+      this._tempoPreparo = tempo;
+  }
+
+  public int getTempoPreparo(){
+    return this._tempoPreparo;
+  }
+
+  public void setTempoPreparo(int tempo){
+    this._tempoPreparo = tempo;
+  }
+
+
+  public void PrepararPedido(){
+
+  }
+
+
+  public void EntregarPedido(){
+    
+  }
+}
