8da1c744bc38cf28df8b01ea15c16e9a02f06878
marcoantoniogsilva <50424217+marcoantoniogsilva@users.noreply.github.com>
2020-11-04 19:43:04 -0300

    Adição da classe Hotdog

---
+++ Hotdog.cs
@@ -0,0 +1,20 @@
+class Hotdog{
+  private int _qtdSalsichas;
+
+  public Hotdog(){
+
+  }
+
+  public void Hotdog(int qtdSalsichas){
+    this._qtdSalsichas = qtdSalsichas;
+  }
+
+  public int getQtdSalsichas(){
+    return this._qtdSalsichas;
+  }
+
+  public void setQtdSalsichas(int qtdSalsichas){
+    this._qtdSalsichas = qtdSalsichas;
+  }
+  
+}
