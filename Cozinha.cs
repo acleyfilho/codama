4a76bca0ec79a85f8c57bca45be80095005992a8
acleyfilho <50428838+acleyfilho@users.noreply.github.com>
2020-11-23 22:09:00 -0300

    Update Cozinha.cs

--- Cozinha.cs
+++ Cozinha.cs
@@ -17,41 +17,6 @@ class Cozinha{
     this._tempoPreparo = tempo;
   }
 
-using System;
-using System.IO;
-namespace Operacoes_IO
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string arquivo = @"C:\dados\macoratti.txt";
-            if (File.Exists(arquivo))
-            {
-                try
-                {
-                    using (StreamReader sr = new StreamReader(arquivo))
-                    {
-                        String linha;
-                        // Lê linha por linha até o final do arquivo
-                        while ((linha = sr.ReadLine()) != null)
-                        {
-                            Console.WriteLine(linha);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            else
-            {
-                Console.WriteLine(" O arquivo " + arquivo + "não foi localizado !");
-            }
-            Console.ReadKey();
-        }
-    }
-}
+
 
 }
