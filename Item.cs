6c5beca84eaebb60e53124ca345353471dd12fef
arthurdeoliveiraribeiro <56887560+arthurdeoliveiraribeiro@users.noreply.github.com>
2020-12-01 17:46:40 -0300

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -42,7 +42,76 @@ public abstract class Item
 
     public void setPreco(int preco)
     {
+        _preco = preco;// Classe abstrata Item
+public abstract class Item
+{
+    // Atributos
+    private string _idItem;
+    private int _preco;
+
+    // Construtores
+    public Item() { }
+
+    public Item(string idItem, int preco)
+    {
+        _idItem = idItem;
         _preco = preco;
+    }
+
+    // Gets e sets
+    public string getIdItem()
+    {
+        return _idItem;
+    }
+
+    public float getPreco()
+    {
+        return _preco;
+    }
+    
+    public void setIdItem(string idItem)
+    {
+        _idItem = idItem;
+    }
+
+    public void setPreco(int preco)
+    {
+        _preco = preco;
+    }
+
+    // Utilização de sobrecarga de operadores (operador +)
+    public static int operator+(Item i1, Item i2)
+    {
+        int soma;
+
+        soma = i1._preco + i2._preco;
+
+        return soma;
+    }
+
+    // Métodos virtuais
+    public virtual string getTipoCarne()
+    {
+        return "";
+    }
+
+    public virtual string getQtdSalsichas()
+    {
+        return "";
+    }
+
+    public virtual string getTipoBebida()
+    {
+        return "";
+    }
+
+    public virtual void setTipoCarne(string tipoCarne) { }
+
+    public virtual void setQtdSalsichas(string qtdSalsichas) { }
+
+    public virtual void setTipoBebida(string tipoBebida) { }
+
+}
     }
 
     public virtual string getTipoCarne()
